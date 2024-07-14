84113a5ca4481511d5ecfd828dd97b5ff5712890
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-07-13 22:32:03 -0500

    Improve operator button behaviors
    When a second operator is pressed after a complete operation has been supplied
    (2 + 2 + ) the previous operation is calculated and displayed in the result
    div. If enter is pressed, the result can be used immediately in a following operation.
    
    Currently pressing a number button after hitting enter does not replace the contents of
    the display window/start a new operation. That needs to be corrected.

--- calculator.js
+++ calculator.js
@@ -20,55 +20,56 @@ const decimal = document.querySelector(".decimal");
 decimal.addEventListener("click", addDecimal);
 
 let array = [];
-var resultDisplaying = false;
+var calculating = false;
+var num_pressed = false;
 
 function updateDisplay(e){
     let string = display.textContent;
     let char = this.textContent;
-    if (resultDisplaying) {
-        string = "0"
-        resultDisplaying = false;
-    }
-    if (string === "0"){
-        if (char !== "0"){
-            string = char;
-        }
+    if (string === "0" || calculating){
+        string = char;
+        calculating = false;
     }
     else{
         string += char;
     }
     display.textContent = string;
+    num_pressed = true;
 }
 
 function clearCalc(e){
     display.textContent = "0";
+    calculating = false;
     array = [];
 }
 
 function opUpdate(e){
-    let num = parseFloat(display.textContent);
-    let ops = "+-×÷"
-    if (ops.includes(array[array.length-1])){
+    let ops = "+-×÷";
+    if (array.length === 2 && num_pressed){
+        calculate(null);
+    }
+    else if (ops.includes(array[array.length-1])){
         array[array.length-1] = this.textContent;
         return;
     }
+    let num = parseFloat(display.textContent);
     array.push(num);
     array.push(this.textContent);
-    display.textContent = "0";
+    calculating = true;
+    num_pressed = false;
 }
 
 function calculate(e){
+    if (!num_pressed) return;
     array.push(parseFloat(display.textContent));
-    let result = array[0];
-    for (let i = 1; i < array.length;i += 2){
-        result = operate(result,array[i],array[i+1]);
-        if(result === null){
-            result = "Oops :(";
-            break;
-        }
+    console.log(array);
+    let result = operate(array[0],array[1],array[2]);
+    if(result === null){
+        result = "Oops :(";
     }
     display.textContent = result;
-    resultDisplaying = true;
+    calculating = false;
+    array = []
 }
 
 function addDecimal(e){
